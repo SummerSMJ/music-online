2a0ab132f6d5b8f7f6edcef00a620b16ba91f845
Administrator <942640644@qq.com>
2020-05-29 17:51:13 +0800

    init

--- app.js
+++ app.js
@@ -3,15 +3,17 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var ejs = require('ejs');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('view engine', 'jade');
+app.engine('html', ejs.__express);
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/music-online', indexRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,50 @@
 var express = require('express');
-var router = express.Router();
+var fs = require('fs');
+var querystring = require('querystring');
 
+var router = express.Router();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('music', {title: 'Express'});
+});
+
+router.get('/json', function (request, response, next) {
+    fs.readFile('/home/projects/music-online/public/json/music.json', function (err, data) {
+        if (err) {
+            console.log(err);
+            response.writeHead(404, {'Content-Type': 'text/html'});
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.write(data.toString());
+        }
+        response.end();
+    });
+});
+
+router.get('/set', function (req, res, next) {
+    var body = "";
+    req.on('data', function (chunk) {
+        body += chunk;
+    });
+
+    req.on('end', function () {
+        body = JSON.stringify(querystring.parse(body));
+        res.writeHead(200, {'Content-Type': 'text/html; charset=utf8'});
+        fs.writeFile('/home/projects/music-online/public/json/music.json', body, function (err) {
+            if (err) {
+                return console.error(err);
+            }
+            console.log("数据写入成功！");
+            fs.readFile('/var/a.json', function (err, data) {
+                if (err) {
+                    return console.error(err);
+                }
+                console.log("文件数据: " + data.toString());
+            });
+        });
+        res.end();
+    });
+
 });
 
 module.exports = router;
